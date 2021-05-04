a8646d4ab6f127ac6a2b06e38e32e8b56350a303
효정이 <w2029@e-mirim.hs.kr>
2021-05-04 20:02:59 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/w2029/chapter4_9ex/MainActivity.java
@@ -0,0 +1,31 @@
+package kr.hs.emirim.w2029.chapter4_9ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imgv;
+    int angle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnRotate = findViewById(R.id.btn_rotate);
+        imgv = findViewById(R.id.imgv);
+
+        btnRotate.setOnClickListener(btnListener);
+    }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            angle += 10;
+            imgv.setRotation(angle);
+        }
+    };
+}
